2d821e5e4e5659134b16c7c70ec5373f23acacb5
KaSahi <kahvisabatino@gmail.com>
2022-11-17 06:59:47 -1000

    Add project files.

---
+++ ORM Dapper/DapperDepartmentRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORM_Dapper
+{
+    public class DapperDepartmentRepository : IDepartmentRepository
+    {
+        private readonly IDbConnection _connection;
+        //Constructor
+        public DapperDepartmentRepository(IDbConnection connection)
+        {
+            _connection = connection;
+        }
+
+        public IEnumerable<Department> GetAllDepartments()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
